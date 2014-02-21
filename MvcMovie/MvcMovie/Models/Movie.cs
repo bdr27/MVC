e58f5f55af504f466b0b3b9a968538186148802b
Brendan Russo <brendanrusso27@gmail.com>
2014-02-21 11:22:27 +1000

    Working on MvcMovie
    http://www.asp.net/mvc/tutorials/mvc-4/getting-started-with-aspnet-mvc4/examining-the-edit-methods-and-edit-view

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { set; get; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Prive { get; set; }
+
+        public class MovieDBContext : DbContext
+        {
+            public DbSet<Movie> Movies { get; set; }
+        }
+    }
+}
